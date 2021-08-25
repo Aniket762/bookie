9c54fba909116b8fe7523ea6012e3895646e130e
Aniket762 <aniketindian8@gmail.com>
2021-08-25 14:12:08 +0530

    Feat:Routes muxed

--- main.go
+++ main.go
@@ -1,15 +1,45 @@
 package main
 
 import (
-	"fmt"
 	// "encoding/json"
-	// "log"
-	// "net/http"
+	"log"
+	"net/http"
+
 	// "math/rand"
 	// "strconv"
-	// "github.com/gorilla/mux"
+	"github.com/gorilla/mux"
 )
 
+// Author model
+type Author struct{
+	FirstName	string `json:"firstname"`
+	LastName	string `json:"lastname"`
+	
+}
+
+// Book model
+type Book struct {
+	ID		string		`json:"id"`
+	Isbn	string		`json:"isbn"`
+	Title	string		`json:"title"`
+	Author	*Author		`json:"author"`
+}
+
+
+
 func main(){
-	fmt.Println("Hey! from Mackie 🥷🏻")
+	// initializing router
+	router := mux.NewRouter()
+
+	// routes
+	router.HandleFunc("/api/books",getBooks).Methods("GET")
+	router.HandleFunc("/api/books/{id}",getBook).Methods("GET")
+	router.HandleFunc("/api/books",createBook).Methods("POST")
+	router.HandleFunc("/api/books/{id}",updateBook).Methods("PUT")
+	router.HandleFunc("/api/books/{id}",deleteBook).Methods("DELETE")
+
+	// running server
+	log.Fatal(http.ListenAndServe(":8080",router))
+
+
 }
